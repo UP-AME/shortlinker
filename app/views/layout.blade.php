f4a65ec84c04a0851c2ad6324216fc9cc30ec3e2
Juan Felipe Coronel <juanfelipe.coronel@gmail.com>
2014-02-08 14:56:46 +0800

    Hackish asset solution

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('html_title') - UP AME Shortlinker</title>
+    {{ HTML::style('/public/css/foundation.css'); }}
+	{{ HTML::script('/public//js/modernizr.js'); }}
     {{ HTML::style('/css/foundation.css'); }}
 	{{ HTML::script('/js/modernizr.js'); }}
   </head>
@@ -60,8 +62,11 @@
 
 	@yield('content')
         
+	{{ HTML::script('/public/js/jquery.js'); }}
+	{{ HTML::script('/public/js/foundation.min.js'); }}
 	{{ HTML::script('/js/jquery.js'); }}
 	{{ HTML::script('/js/foundation.min.js'); }}
+
     <script>
       $(document).foundation();
     </script>
